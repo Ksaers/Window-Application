5f6acae23ace4d47151b771a6466b501acd937df
Mihalev Andrey Evgenevich <61120576+Ksaers@users.noreply.github.com>
2023-05-29 11:37:23 +0300

    Добавьте файлы проекта.

---
+++ DemoWpf/Winds/MainWIn.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1.Winds
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWIn.xaml
+    /// </summary>
+    public partial class MainWIn : Window
+    {
+        public MainWIn()
+        {
+            InitializeComponent();
+        }
+        private void Win1Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.ShowDialog();
+        }
+
+        private void Win2Click(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2();
+            window2.ShowDialog();
+        }
+
+        private void Win3Click(object sender, RoutedEventArgs e)
+        {
+            Window3 window3 = new Window3();
+            window3.ShowDialog();
+        }
+
+        private void Win4Click(object sender, RoutedEventArgs e)
+        {
+            Window4 window4 = new Window4();
+            window4.ShowDialog();
+        }
+
+        private void Win5Click(object sender, RoutedEventArgs e)
+        {
+            Window5 window5 = new Window5();
+            window5.ShowDialog();
+        }
+    }
+}
---
+++ Style/View/FormWin.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Style.View
+{
+    /// <summary>
+    /// Логика взаимодействия для FormWin.xaml
+    /// </summary>
+    public partial class FormWin : Window
+    {
+        public (string Rus, string Eng)[] values =
+        {
+            ("Форма ввода DemoStyles", "Forms input Demostyle"),
+            ("Фамилия", "Surname"),
+            ("Отчество", "Middle name"),
+            ("Адрес проживания", "Residental address"),
+            ("Год рождения", "Year of birth"),
+            ("Образование", "Education"),
+            ("Национальность", "Education"),
+            ("Сохранить", "Save"),
+            (" Выход ", " Exit ")
+        };
+        private void CheckLang(object sender, RoutedEventArgs e)
+        {
+            RadioButton radio = sender as RadioButton;
+            if (radio == null)
+            {
+                return;
+            }
+            switch (radio.Name)
+            {
+                case "Eng":
+                    L1.Content = values[0].Eng;
+                    L2.Content = values[1].Eng;
+                    L3.Content = values[2].Eng;
+                    L4.Content = values[3].Eng;
+                    L5.Content = values[4].Eng;
+                    L6.Content = values[5].Eng;
+                    L7.Content = values[6].Eng;
+                    L8.Content = values[7].Eng;
+                    break;
+                case "Rus":
+                    L1.Content = values[0].Rus;
+                    L2.Content = values[1].Rus;
+                    L3.Content = values[2].Rus;
+                    L4.Content = values[3].Rus;
+                    L5.Content = values[4].Rus;
+                    L6.Content = values[5].Rus;
+                    L7.Content = values[6].Rus;
+                    L8.Content = values[7].Rus;
+                    break;
+            }
+        }
+        public FormWin()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickSave(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClickExit(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
